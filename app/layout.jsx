81065c5c74176ac21222bb432677b83aea0a27a7
shaikhazrath <shaikhazrathali123@gmail.com>
2025-01-02 18:19:46 +0530

    added replied

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,8 +4,8 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "Twitter-like Posting",
-  description: "A simple Twitter-like posting system",
+  title: "echo-ai",
+  description: "Welcome to ECHO-AI, a unique social media platform where you can share your thoughts, situations, or anything you usually post on social media. Here, AI with different perspectives will comment and help you handle situations, providing you with better insights from various mindsets",
 }
 
 export default function RootLayout({
